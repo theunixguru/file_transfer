093f0e95e1f7ef36a2074d2f463b0d4d5361d63d
Denys Vorobyov <denys@vorobyovonline.com>
2017-02-07 09:20:55 +0300

    Added SSH file transfer functional test

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,14 +1,26 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\BeforeFeatureScope;
+use Behat\Behat\Hook\Scope\AfterFeatureScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
+
+require_once(__DIR__.'/../../lib/file_transfer_lib.php');
+
+use FileTransfer as FT;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+    const TEST_FILENAME = 'dump.tar.gz';
+    const TEST_FILE_LOCATION = '/var/www';
+
     /**
      * Initializes context.
      *
@@ -19,4 +31,61 @@ class FeatureContext implements Context
     public function __construct()
     {
     }
+
+    /** @BeforeScenario */
+    public function before(BeforeScenarioScope $scope)
+    {
+        $this->testData = [
+            'call_data' => []
+        ];
+    }
+
+    /**
+     * @Given a user has valid user credentials on a remote server
+     */
+    public function aUserHasValidUserCredentialsOnARemoteServer()
+    {
+        $this->testData['user_credentials'] = [
+            'username' => 'dennis',
+            'password' => '9!m8!yKG)N5sf\lk+CfH+b2N',
+            'host' => '192.168.56.12'
+        ];
+    }
+
+    /**
+     * @When the user connects to the server
+     */
+    public function theUserConnectsToTheServer()
+    {
+        $factory = new FT\Factory();
+        $user_data = $this->testData['user_credentials'];
+        
+        $this->testData['session'] = $factory->getConnection('ssh',
+            $user_data['username'],
+            $user_data['password'],
+            $user_data['host']
+        );
+    }
+
+    /**
+     * @When initiates the file download
+     */
+    public function initiatesTheFileDownload()
+    {
+        $this->testData['session']->cd(self::TEST_FILE_LOCATION)
+            ->download(self::TEST_FILENAME)
+            ->close();
+    }
+
+    /**
+     * @Then a matching local file must be created
+     */
+    public function aMatchingLocalFileMustBeCreated()
+    {
+        PHPUnit_Framework_Assert::assertFileExists(
+            self::TEST_FILENAME,
+            'The requested file is downloaded'
+        );
+    }
+
 }
---
+++ features/ssh_file_transfer.feature
@@ -0,0 +1,11 @@
+Feature: A class user should be able to utilize SSH connection
+         for file transfers
+
+
+Scenario: A user downloads a file via SSH connection
+
+    Given a user has valid user credentials on a remote server
+    When the user connects to the server
+    And initiates the file download
+    Then a matching local file must be created
+
